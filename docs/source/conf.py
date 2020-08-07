986c60c9d46be73b74d7f17efc24fc36f6f42cb0
Mark <m1pearson@me.com>
2020-08-07 13:48:43 +0100

    Add auto_mock_modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '0.0.5'
 # ones.
 extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
 
-autodoc_mock_imports = ['matplotlib', 'numpy']
+autodoc_mock_imports = ['matplotlib', 'numpy', 'matplotlib.pyplot', 'warnings']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
